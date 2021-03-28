deb6731e5de501eb796e46756b1851ee46d8d09c
ErastKoliasa <71034949+ErastKoliasa@users.noreply.github.com>
2021-03-28 20:15:33 +0300

    Delete Practika 5 directory

--- Practika 5/Generator.py
+++
@@ -1,9 +0,0 @@
-from Validation import Validator
-valid = Validator()
-class GeneratorWithIterator():
-    def insert(self, list):
-        print("Введіть позицію для вставки:", end='')
-        pos = valid.isInteger()
-        element = next(iter)
-        list.InsertYourValues(pos, element)
-        return list
